a416f61c38e2c50985b9ec793b8fd8b5b3ce3ce6
AggieWB <usmaggie23@gmail.com>
2020-08-20 11:17:59 -0700

    Uninstall moment timezone, and install/require moment

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const moment = require('moment-timezone');
+const moment = require('moment');
 const { User, Exercise } = require('./models/User.js');
 
 const cors = require('cors');
